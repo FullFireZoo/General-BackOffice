772f693851a7ccbfc072a1abcecf7661c4c4dd56
docteurseo <you@example.com>
2022-06-07 16:26:25 +0200

    add get

--- admin.js
+++ admin.js
@@ -1,17 +1,26 @@
 import {gf} from './utils/fetch.js';
 import {create} from './pages/create.js';
 
-document.getElementById('nav').addEventListener('click',function(e){
-    e.preventDefault();
-    console.log(e.target.textContent);
+document.getElementById('menu').addEventListener('click',function(e){
+    e.preventDefault()
+     gf('http://localhost:3000/api/v1/'+ e.target.textContent, display)
+    
+})
 
-    if(e.target.textContent === 'Create'){
-        create('body')
+function display(data) {
+   
     
 
+    for(let info in data) {
+        console.log(info)
+        const li = document.createElement('li');
+
+        li.innerHTML = `<li>${data[info].id}</li>`
+        document.querySelector('ul').appendChild(li);
+
     }
-    document.getElementById('app').textContent = e.target.textContent;
-})
+
+}
 
 
 
